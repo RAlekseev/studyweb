2f7fcbbf8f79d96412b0327ca62f2cda279f1530
RAlekseev <roma99@bk.ru>
2019-03-11 19:20:14 +0300

    [E] alert.js JavaScript задание 2.10 Функция проверки на то, целое ли число

--- alert.js
+++ alert.js
@@ -1,4 +1,13 @@
-var admin;
-var name = "Василий";
-admin = name;
-alert(admin);
+
+var num1 = 3;
+
+function isInteger(num){
+    if ((num^0) === num){
+        alert("Целое");
+    }
+    else{
+        alert("Нецелое");
+    }
+}
+
+isInteger(num1);
