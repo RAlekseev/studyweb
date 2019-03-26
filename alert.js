5b55c44c50bf30eaeb9df27a25072edad64541c5
RAlekseev <roma99@bk.ru>
2019-03-26 16:00:22 +0300

    [E] alert.js JavaScript задания 4.4

--- alert.js
+++ alert.js
@@ -1,36 +1,11 @@
-// Первое задание приведение первой буквы к верхнему регистру//
+// Первое задание  ассоциативные массивы//
 
-function ucFirst(str){
-    firstChar = str.charAt(0);
-    return firstChar.toUpperCase() + str.slice(1);
-}
+user = {};
+user.name = 'Вася';
+user.surname = 'Петров';
+user.name = 'Сергей';
+delete user.name;
 
-alert( ucFirst('Вася') );
 
-// Второе задание проверка на спам/
 
-function checkSpam(str){
-    strLow = str.toLowerCase();
-    return !!( ~strLow.indexOf('viagra') || ~strLow.indexOf('xxx') )
-}
 
-alert( checkSpam("innocent rabbit"));
-
-// Третье задание, случайное число от min до maks //
-
-function truncate(str, maxlength) {
-    if (str.length > maxlength){
-        return str.slice(0,maxlength - 3) + '...';
-    }
-    return str;
-}
-
-alert( truncate("Вот, что мне", 20));
-
-// Четвертое задание: Выделить число //
-
-function extractCurrencyValue(str){
-    return parseInt(str.slice(1));
-}
-
-alert( extractCurrencyValue('$120'));
